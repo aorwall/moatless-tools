9ec9181f092e5a4d23d536751f6a0494f7b4677d
Albert Örwall <albert@moatless.ai>
2023-08-25 21:30:36 +0200

    Refactor Typescript parser

--- codeblocks/parser/javascript.py
+++ codeblocks/parser/javascript.py
@@ -5,7 +5,7 @@ from tree_sitter import Node
 
 from codeblocks.codeblocks import CodeBlockType, CodeBlock
 
-from codeblocks.parser.parser import CodeParser, _find_type, find_nested_type, find_type
+from codeblocks.parser.parser import CodeParser, find_nested_type, find_type
 
 class_node_types = [
     "class_declaration",
@@ -29,9 +29,9 @@ statement_node_types = [
 block_delimiters = [
     "{",
     "}",
+    ";",
     "(",
-    ")",
-    ";"
+    ")"
 ]
 
 
@@ -95,37 +95,23 @@ class JavaScriptParser(CodeParser):
     def find_arrow_func(self, node: Node):
         arrow_func = find_nested_type(node, "arrow_function")
         if arrow_func:
-            block_delimiter = find_nested_type(arrow_func, "{")
-            if block_delimiter:
-                return block_delimiter
-
-            delimiter = find_nested_type(arrow_func, "=>")
-            return delimiter.next_sibling
+            arrow = find_nested_type(arrow_func, "=>")
+            if arrow:
+                block_delimiter = find_nested_type(arrow.next_sibling, "{")
+                if block_delimiter:
+                    return block_delimiter
+                else:
+                    return arrow.next_sibling
         return None
 
     def find_first_child(self, node: Node) -> Optional[Node]:
+        if node.type in ["binary_expression"]:
+            return None
+
         if node.type in ["expression_statement"]:
             node = node.children[0]
 
-        block = find_type(node, ["class_body", "statement_block", "object", "object_type", "object_pattern", "switch_body", "jsx_expression"])
-        if block:
-            delimiter = find_type(block, ["{"])
-            if delimiter:
-                return delimiter
-            return block.children[0]
-
-        call_func = find_type(node, ["call_expression"])
-        if call_func:
-            arrow_func = self.find_arrow_func(call_func);
-            if arrow_func:
-                return arrow_func
-
-        if node.type in ["switch_case", "switch_default"]:
-            delimiter = find_type(node, [":"])
-            if delimiter:
-                return delimiter
-
-        if node.type in ["variable_declarator", "variable_declaration", "lexical_declaration", "call_expression", "new_expression"]:
+        if node.type in ["variable_declarator", "variable_declaration", "lexical_declaration", "call_expression", "new_expression", "type_alias_declaration"]:
             arrow_func = self.find_arrow_func(node)
             if arrow_func:
                 return arrow_func
@@ -138,60 +124,40 @@ class JavaScriptParser(CodeParser):
                 if end_delimiter:
                     return end_delimiter
 
-        if node.type in ["parenthesized_expression", "program", "statement_block"]:
+        block = find_type(node, ["class_body", "enum_body", "statement_block", "object", "object_type", "object_pattern", "switch_body", "jsx_expression"])
+        if block:
+            delimiter = find_type(block, ["{"])
+            if delimiter:
+                return delimiter
+            return block.children[0]
+
+        call_func = find_type(node, ["call_expression"])
+        if call_func:
+            arrow_func = self.find_arrow_func(call_func);
+            if arrow_func:
+                return arrow_func
+
+        parenthesized_expression = find_type(node, ["parenthesized_expression"])
+        if parenthesized_expression:
+            delimiter = find_type(parenthesized_expression, ["("])
+            if delimiter:
+                return delimiter
+            return parenthesized_expression.children[0]
+
+        if node.type in ["switch_case", "switch_default"]:
+            delimiter = find_type(node, [":"])
+            if delimiter:
+                return delimiter
+
+        if node.type in ["program", "statement_block"]:
             return node.children[0]
 
         if node.type in ["return_statement", "jsx_element", "call_expression", "assignment_expression"]:
             return node.children[1]
+
 
         return None
 
     def get_child_nodes(self, node: Node) -> List[Node]:
         if node.type == "program":
             return node.children
-
-    def parse_code(self, content_bytes: bytes, node: Node, start_byte: int = 0) -> CodeBlock:
-        pre_code = content_bytes[start_byte:node.start_byte].decode(self.encoding)
-
-        block_type = self.get_block_type(node)
-
-        first_child = self.find_first_child(node)
-
-        children = []
-
-        if first_child:
-            end_byte = first_child.start_byte
-            end_line = node.end_point[0]
-        else:
-            end_byte = node.end_byte
-            end_line = node.end_point[0]
-
-        code = content_bytes[node.start_byte:end_byte].decode(self.encoding)
-
-        next_node = first_child
-        while next_node:
-            children.append(self.parse_code(content_bytes, next_node, start_byte=end_byte))
-            end_byte = next_node.end_byte
-            if next_node.next_sibling:
-                next_node = next_node.next_sibling
-            else:
-                next_node = self.get_parent_next(next_node, node)
-
-        return CodeBlock(
-            type=block_type,
-            tree_sitter_type=node.type,
-            start_line=node.start_point[0],
-            end_line=end_line,
-            pre_code=pre_code,
-            content=code,
-            children=children,
-            language=self.language
-        )
-
-    def get_parent_next(self, node: Node, orig_node: Node):
-        if node != orig_node:
-            if node.next_sibling:
-                return node.next_sibling
-            else:
-                return self.get_parent_next(node.parent, orig_node)
-        return None
--- tests/test_javascript_parser.py
+++ tests/test_javascript_parser.py
@@ -10,7 +10,7 @@ def test_javascript_treesitter_types():
         expected_tree = f.read()
 
     codeblock = parser.parse(content)
-    print(codeblock.to_tree(include_tree_sitter_type=False))
+    print(codeblock.to_tree(include_tree_sitter_type=True))
 
     assert codeblock.to_tree() == expected_tree
     assert codeblock.to_string() == content
