d289bd94c5961d38b38b08c5fb42f1020a1a5f8a
Albert Örwall <albert@platform24.com>
2023-08-31 10:35:14 +0200

    Bump to version 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="codeblocks-gpt",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     #entry_points={
     #    'console_scripts': [
