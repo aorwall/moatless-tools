199f5f07a11c23a4c7b5a93e275f09bd14dcf044
Albert Örwall <albert@moatless.ai>
2025-04-23 08:06:18 +0200

    Dont import redis

--- moatless/eventbus/__init__.py
+++ moatless/eventbus/__init__.py
@@ -1,5 +1,4 @@
 from moatless.eventbus.base import BaseEventBus
 from moatless.eventbus.local_bus import LocalEventBus
-from moatless.eventbus.redis_bus import RedisEventBus
 
-__all__ = ["BaseEventBus", "LocalEventBus", "RedisEventBus"]
+__all__ = ["BaseEventBus", "LocalEventBus"]
